a8a39ae376695b425e85ce8f62a4ce2e5fca5504
THEO MATAS <matas.racer@gmail.com>
2020-12-15 22:49:50 +0100

    [PROM] add localhost/metrics

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Created on Wed Nov  4 14:52:48 2020
 """
 
 
-from flask import Flask, render_template, request, jsonify, send_file, redirect, url_for,make_response
+from flask import Flask, render_template, request, jsonify, send_file, redirect, url_for,make_response, Response
 import webbrowser
 from config import Projet,Version
 from datetime import datetime
@@ -15,7 +15,55 @@ from get_tweet import get_20_best
 
 
 
+
 app = Flask(__name__)
+
+from prometheus_client import (generate_latest, CONTENT_TYPE_LATEST )
+import time
+from prometheus_client import Counter, Histogram
+from prometheus_client import start_http_server
+
+FLASK_REQUEST_LATENCY = Histogram('flask_request_latency_seconds', 'Flask Request Latency',
+                                  ['method', 'endpoint'])
+FLASK_REQUEST_COUNT = Counter('flask_request_count', 'Flask Request Count',
+                              ['method', 'endpoint', 'http_status'])
+
+
+def before_request():
+    request.start_time = time.time()
+
+
+def after_request(response):
+    request_latency = time.time() - request.start_time
+    FLASK_REQUEST_LATENCY.labels(request.method, request.path).observe(request_latency)
+    FLASK_REQUEST_COUNT.labels(request.method, request.path, response.status_code).inc()
+
+    return response
+
+
+def monitor(app):
+    app.before_request(before_request)
+    app.after_request(after_request)
+
+
+@app.route('/metrics')
+def metrics():
+    """Flask endpoint to gather the metrics, will be called by Prometheus."""
+    #monitor(app)
+    #print(generate_latest().decode().split('\n').index())
+    return Response(generate_latest(),
+                    mimetype=CONTENT_TYPE_LATEST)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -81,4 +129,5 @@ def bad_request(e):
 if __name__ == '__main__':
     #webbrowser.open('http://127.0.0.1:5000/', new=2)
     print("Lancement de l'app")
+    monitor(app)
     app.run(host='0.0.0.0',threaded=True)
